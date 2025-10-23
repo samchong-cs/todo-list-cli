f5df6846923a2b4d40080f04649041654b5d1ec3
https://github.com/samchong-cs <chongsamuel626@gmail.com>
2025-10-23 21:17:54 +0800

    Add basic project file

---
+++ todo.py
@@ -0,0 +1,10 @@
+# To-Do List Application
+# Author: Samuel Chong
+# Started: 23 October 2025
+
+def main():
+    print("Welcome to my to-do app!")
+    print("New features will be added soon!")
+
+if __name__  == main ():
+    main()
