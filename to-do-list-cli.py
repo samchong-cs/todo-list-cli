906bc223652348ad7ce4c80d2c31c7bd510b4128
https://github.com/samchong-cs <chongsamuel626@gmail.com>
2025-10-26 21:38:23 +0800

    Adding the add_task function primarily

--- to-do-list-cli.py
+++ to-do-list-cli.py
@@ -1,28 +1,50 @@
 # Created on 26 October 2025
 # Author: Samuel Chong
 
-# *Menu*:
+# *List of tasks:*
+task_list = []
 
-# Printing out user options:
-print("1. Add task")
-print("2. Delete task")
-print("3. Editing task")
-print("4. Mark task complete")
-print("5. Exit application")
+# *Created functions:*
+def add_task():
+    print("\nNOTE: Type in the tasks one by one, press enter after each. Write 'DONE' to complete adding tasks.")
+    while True:
+        task_input = input("\nType in your task: ")
+        # Adds the task to user list based on what user enters:
+        task_list.append(task_input)
+        if task_input == "DONE":
+            print(f"\nThis is your task list: {task_list}.")
+            print("\nYou have successfully finished adding tasks!")
+            menu()
+            break
 
-# Setting the variable for the task user selects:
-task = int(input("Enter the task number: "))
+def menu():
+    # Printing out user options:
+    print("\n1. Add task")
+    print("2. Delete task")
+    print("3. Editing task")
+    print("4. Mark task complete")
+    print("5. Exit application")
 
-# Looping the menu until user exits the application:
-while True:
-    if task != 5:
-        print("1. Add task")
-        print("2. Delete task")
-        print("3. Editing task")
-        print("4. Mark task complete")
-        print("5. Exit application")
-        task = int(input("Enter the task number: "))
+    # Setting the variable for the task user selects:
+    task = int(input("Enter the task number from 1 to 5: "))
 
-    else:
-        print("You have succesfully exited the application.")
-        break
+    # Looping the menu until user exits the application:
+    while True:
+        # Applying the add task function from above:
+        if task == 1:
+            add_task()
+        
+        elif task != 5:
+            print("1. Add task")
+            print("2. Delete task")
+            print("3. Editing task")
+            print("4. Mark task complete")
+            print("5. Exit application")
+            task = int(input("Enter the task number: "))
+
+        else:
+            print("You have successfully exited the application.")
+            break
+
+# *Calling the menu fucntion*:
+menu()
