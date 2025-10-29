6ea6e5e7cb8f6a6b884533a923c6a7f6241ed422
https://github.com/samchong-cs <chongsamuel626@gmail.com>
2025-10-29 22:00:41 +0800

    Created delete task function

--- to-do-list-cli.py
+++ to-do-list-cli.py
@@ -6,6 +6,59 @@ task_list = []
 next_id = 1
 
 # *Created functions:*
+
+# Function 1:
+def view_tasks():
+    if len(task_list) == 0:
+        print("\nNo tasks yet!")
+        menu()
+        return
+    
+    print("\nYOUR TASKS:")
+    for task in task_list:
+        print(f"[{task['id']}] {task['task']}")
+    menu()
+
+# Function 2:
+def menu_message():
+    # Printing out user options:
+    print("\n1. Add task")
+    print("2. Delete task")
+    print("3. Editing task")
+    print("4. Mark task complete")
+    print("5. View task lists")
+    print("6. Exit application")
+
+# Function 3:
+def menu():
+    menu_message()
+    # Setting the variable for the task user selects:
+    task = int(input("Enter the task number from 1 to 6: "))
+
+    # Looping the menu until user exits the application:
+    while True:
+        # Applying the add task function from above:
+        if task == 1:
+            add_task()
+            break
+
+        elif task == 2:
+            delete_task()
+            break
+        
+        elif task == 5:
+            view_tasks()
+            break
+        
+        elif task != 6:
+            menu_message()
+            task = int(input("Enter the task number: "))
+
+        else:
+            print("You have successfully exited the application.")
+            break
+
+# Function 4:
 def add_task():
     # To make sure that next_id variable is able to linked to this function:
     global next_id
@@ -27,45 +80,28 @@ def add_task():
             # Automatically updating the ID number of the next task entered:
             next_id = next_id + 1
     
-    # Printing the list as a 
+    # Printing the dictionaries as a list
     print(f"\nThis is the tasks list you entered: {task_list}. You successfully finished entering tasks.")
     menu()
 
-def view_tasks():
+# Function 5:
+def delete_task():
     print(task_list)
 
-def menu_message():
-    # Printing out user options:
-    print("\n1. Add task")
-    print("2. Delete task")
-    print("3. Editing task")
-    print("4. Mark task complete")
-    print("5. View task lists")
-    print("6. Exit application")
+    if len(task_list) == 0:
+        print("No tasks to delete!")
 
-def menu():
-    menu_message()
-    # Setting the variable for the task user selects:
-    task = int(input("Enter the task number from 1 to 6: "))
+    id_to_delete = int(input("Enter the ID number of the task you want to delete: "))
 
-    # Looping the menu until user exits the application:
-    while True:
-        # Applying the add task function from above:
-        if task == 1:
-            add_task()
-            break
-        
-        elif task == 5:
-            view_tasks()
-            break
-        
-        elif task != 6:
-            menu_message()
-            task = int(input("Enter the task number: "))
-
-        else:
-            print("You have successfully exited the application.")
-            break
-
+    for task in task_list:
+        if task["id"] ==  id_to_delete:
+            task_list.remove(task)
+            print(f"Deleted task for ID {id_to_delete}")
+            menu()
+            return
+    
+    print("Task ID not found! Please retry!")
+    menu()
+    
 # *Calling the menu function*:
 menu()
